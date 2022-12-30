71a08f2e98f035334e6da365873973e081f21e82
mkmasudrana806 <mkmasudrana806@gmail.com>
2022-12-30 15:13:14 +0600

    exercise, tutorials and courses api are are created in backend

--- index.js
+++ index.js
@@ -6,11 +6,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 const categories = require("./data/categories.json");
 const categoryTopics = require("./data/languages_details.json");
+const exercise_categories = require("./data/exercise_categories.json");
+const exercises = require("./data/exercises.json");
+const courses = require("./data/courses.json");
 
 app.get("/", (req, res) => {
   res.send("programming server is running");
 });
 
+//below api is for exercise
+app.get("/exercise-categories", (req, res) => {
+  res.send(exercise_categories);
+});
+
+app.get("/exercise-category/:id", (req, res) => {
+  const id = req.params.id;
+  const exercise = exercises.filter((e) => e.exercise_id === id);
+  res.send(exercise);
+});
+
+// below API is for tutorial
 app.get("/categories", (req, res) => {
   res.send(categories);
 });
@@ -27,6 +42,17 @@ app.get("/topic-details/:id", (req, res) => {
   const id = req.params.id;
   const topic = categoryTopics.find((t) => t._id === id);
   res.send(topic);
+});
+
+// below API is for courses
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.filter((c) => c.course_id === id);
+  res.send(course);
 });
 
 app.listen(port, () => {
